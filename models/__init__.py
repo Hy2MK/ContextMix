7bcd1244d9257ad7f90d4e4d21d1a7ebd041272e
Hy2MK <140177940+Hy2MK@users.noreply.github.com>
2024-02-06 02:25:37 +0900

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+# from models.shake_pyramidnet import ShakePyramidNet
+from models.pyramidnet_shake import ShakePyramidNet
+from models.pyramidnet import PyramidNet
+from models.resnet import ResNet
